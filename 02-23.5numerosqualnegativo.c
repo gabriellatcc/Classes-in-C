19a6d6949bfb6afad86c502c9e228f3431938a6b
Gabriella Tavares Costa Corrêa <85166157+6b11h@users.noreply.github.com>
2024-08-22 13:39:36 -0300

    Update 02-23.5numerosqualnegativo.c

--- 02-23.5numerosqualnegativo.c
+++ 02-23.5numerosqualnegativo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int n,i,neg=0; //o n é numero, i é o controle do for e o neg é o contador de numeros negativos
+    int n,i,neg=0;
         for(i=1;i<=5;i++)
         {
             printf("digite um numero: ");
