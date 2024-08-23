12a2e1ea17fe8f5e562db817430d2e7d83058420
Gabriella Tavares Costa Corrêa <85166157+6b11h@users.noreply.github.com>
2024-08-23 04:28:47 -0300

    Update 02-23.factorial.c

--- 02-23.factorial.c
+++ 02-23.factorial.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int n,i,fat=1;
         printf("digite um numero: ");
         scanf("%d",&n);
-            for(i=1;i<=n;i++){
+            for(i=1;i<=n;i++)
+            {
                 fat*=i;
             }
             printf("seu numero fatorado e: %d\n",fat);
