34e8b9bb28bc526f9c8a50f87db6c7bbcb678790
Gabriella Tavares Costa Corrêa <85166157+6b11h@users.noreply.github.com>
2024-08-24 11:14:50 -0300

    Update negativenum.c

--- codes/negativenum.c
+++ codes/negativenum.c
@@ -4,13 +4,13 @@ int main()
     int n,i,neg=0;
         for(i=1;i<=5;i++)
         {
-            printf("digite um numero: ");
+            printf("Enter a number: ");
             scanf("%d",&n);
                 if(n<0)
                 {
                         neg+=1;
                 }
         }
-        printf("numeros negativos: %d", neg);
+        printf("Negative numbers: %d", neg);
     return(0);
 }
