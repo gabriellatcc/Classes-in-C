44760cc3954061889e40099eedb38d25205561d8
Gabriella Tavares Costa Corrêa <85166157+6b11h@users.noreply.github.com>
2024-08-24 11:23:22 -0300

    Add files via upload

---
+++ codes/02-28.deusvetudo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int v, t, d;
+        printf("Enter the velocity value: ");
+        scanf("%d",&v);
+        printf("Enter the time value: ");
+        scanf("%d",&t);
+        d=v*t;
+        printf("Distance: %d\n", d);
+return 0;
+}
