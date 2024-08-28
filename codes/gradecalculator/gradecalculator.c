a578600787dabc9a148dab99026e8cdf099018a1
Gabriella T. C. Corrêa <85166157+gabriellatcc@users.noreply.github.com>
2024-08-28 18:40:12 -0300

    Create gradecalculator.c

---
+++ codes/gradecalculator/gradecalculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    float g1,g2,average;
+    printf("ENTER THE FIRST GRADE: ");
+    scanf("%f",&g1);
+    printf("ENTER THE SECOND GRADE: ");
+    scanf("%f",&g2);
+    average=(n1+n2)/2;
+    printf("Final value: %.2f.",average);
+    if(average>=6)
+    {
+        printf("Student passed.\n");
+    }
+    else
+    {
+        printf("Student failed.\n");
+    }
+return(0);
+}
