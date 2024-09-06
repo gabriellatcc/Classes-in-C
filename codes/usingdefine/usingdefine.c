9aacbe4c6a046e0754cc1ce08ab15abc1992796c
Gabriella T. C. Corrêa <85166157+gabriellatcc@users.noreply.github.com>
2024-09-06 07:28:32 -0300

    Create usingdefine.c

---
+++ codes/usingdefine/usingdefine.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    #define MAX 100
+    int display()
+    {
+    return (MAX);
+    }
+    int main()
+    {
+        int x=1,const;
+            printf("X value= %d\n",x);
+            const=exibir();
+            printf("X value Max= %d",const);
+    return(0);
+    }
