ed29c2bec68680e9b742b0258c09a6545f39788a
Gabriella T. C. Corrêa <85166157+gabriellatcc@users.noreply.github.com>
2024-09-09 19:35:54 -0300

    Create displayvalues.c

---
+++ codes/displayvalues/displayvalues.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int display()
+{
+    int j=10;
+return(j);
+}
+int main()
+{
+    int x=1, j=display();
+    printf("X value= %d\n",x);
+    printf("J value= %d\n",j);
+return(0);
+}
