44aaee24fe8fe4ace532c2c01349748c260e0783
Gabriella T. C. Corrêa <85166157+gabriellatcc@users.noreply.github.com>
2024-09-12 16:36:13 -0300

    Create entervaluesusingfor.c

---
+++ codes/entervaluesusingfor/entervaluesusingfor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int array[5],forcontrol;
+        for(forcontrol=0;forcontrol<=4;forcontrol++)
+        {
+            printf("Enter any integer: ");
+            scanf("%d",&array[forcontrol]);
+        }
+return(0);
+}
